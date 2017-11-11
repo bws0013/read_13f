9d842b6ab3930e20ad6273b6713af4eaff4e83d7
Ben <i@auburn.edu>
2017-11-10 19:40:16 -0600

    Added the ability to get the cusip by guessing the location using the offset of 9 character sequences.

--- src/main/java/com/ben/smith/reader/file_processors.java
+++ src/main/java/com/ben/smith/reader/file_processors.java
@@ -1,7 +1,8 @@
 package com.ben.smith.reader;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.ben.smith.reader.read_unknown_file.read_file;
 
@@ -15,10 +16,68 @@ public class file_processors {
         List<String> valuable_lines = read_old_1("./storage/old_1.txt");
 //        process_old_1(valuable_lines);
 
-        int smallest = get_name_length(valuable_lines);
-        System.out.println(smallest);
+        int guessed_offset = collect_possible_cusip_offsets(valuable_lines);
+        print_cusip_at_offset(guessed_offset, valuable_lines);
+
+//        int smallest = get_name_length(valuable_lines);
+//        System.out.println(smallest);
 
     }
+
+    // Get/Print the cusips at a particular offset (assume 9 char cusip)
+    public static void print_cusip_at_offset(int offset, List<String> lines) {
+
+        for(String line : lines) {
+
+            System.out.println(line.substring(offset, offset + 9));
+
+        }
+
+    }
+
+
+
+
+
+    // Analyze the frequency of 9 character sequences to determine the cusip.
+    public static int collect_possible_cusip_offsets(List<String> lines) {
+
+        Map<Integer, Integer> offset_to_count = new HashMap<>();
+
+        for(String line : lines) {
+            List<Integer> possible_offsets = regex_offset(line);
+            for(int offset : possible_offsets) {
+                if(offset_to_count.containsKey(offset)) {
+                    int count = offset_to_count.get(offset);
+                    count++;
+                    offset_to_count.put(offset, count);
+                } else {
+                    offset_to_count.put(offset, 1);
+                }
+            }
+        }
+
+        List<Integer> offsets = new ArrayList<>(offset_to_count.keySet());
+
+        if(offsets.size() == 0) {
+            System.out.println("No offsets found!");
+            System.exit(1);
+        }
+
+        Collections.sort(offsets);
+
+
+        int offset_with_highest_count = offsets.get(0);
+
+        for(int i = 1; i < offsets.size(); i++) {
+            if(offset_to_count.get(offsets.get(i)) > offset_to_count.get(offset_with_highest_count)) {
+                offset_with_highest_count = offsets.get(i);
+            }
+        }
+
+        return offset_with_highest_count;
+    }
+
 
     public static List<String> read_old_1(String filename) {
 
@@ -86,6 +145,24 @@ public class file_processors {
         }
         return smallest;
     }
+
+    public static List<Integer> regex_offset(String line) {
+
+        List<Integer> found_offsets = new ArrayList<>();
+
+        String pattern = "[A-Za-z0-9]{9}";
+        Pattern finder = Pattern.compile(pattern);
+        Matcher matcher = finder.matcher(line);
+
+
+        while (matcher.find()) {
+            found_offsets.add(matcher.start());
+        }
+
+        return found_offsets;
+    }
+
+
 }
 
 
